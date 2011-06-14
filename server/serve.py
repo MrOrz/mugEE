02ec3fc9235f45886263c88159effcf9e671df38
AZ Huang <aitjcize@gmail.com>
2011-06-14 20:43:16 +0800

    Image rotate now working. (Place Image dir under server/Images/{up/down})

--- server/serve.py
+++ server/serve.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 
+import cv
 import re
 import socket
 import sys
@@ -15,7 +16,24 @@ class ConnectionNotEstablished(Exception):
 
 class DIPDemo(object):
     def __init__(self):
+        self.sock = None
         self.client = None
+        self.phi_list = ['%02d00' % i for i in range(0, 36)]
+        self.theta_list = ['0000', '0070', '0139', '0208', '0277', '0347',
+                           '0416', '0485', '0554', '0624', '0693', '0762',
+                           '0831', '0900']
+        self.phi = 0
+        self.theta = 0
+
+        # Initialize OpenCV
+        cv.NamedWindow("DIP", False)
+        cv.ResizeWindow("DIP", 800, 600)
+
+    def start(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind(("", 9999))
+        self.sock.listen(5)
 
     def connect(self):
         handshake = '\r\n'.join(['HTTP/1.1 101 Web Socket Protocol Handshake',
@@ -25,12 +43,7 @@ class DIPDemo(object):
                                  'Sec-WebSocket-Location: ws://localhost:9999/',
                                  '', '%s'])
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(("", 9999))
-        sock.listen(5)
-
-        self.client, address = sock.accept()
+        self.client, address = self.sock.accept()
         header = self.client.recv(4096)
         if header.find('\r\n\r\n') != -1:
             self._parse_header(header)
@@ -47,6 +60,9 @@ class DIPDemo(object):
             self.client.send(handshake % challenge)
         else:
             raise ConnectionFailed
+
+    def close(self):
+        self.client.close()
 
     def _parse_header(self, header):
         self.header_dict = { i[0]: i[1] for i in
@@ -76,17 +92,36 @@ class DIPDemo(object):
 
         self.validated = validated
 
+        if validated:
+            self.phi = (self.phi + int(validated[0].split(' ')[0])) % 35
+            self.theta = (self.theta + int(validated[0].split(' ')[1])) % 12
+
     def send(self):
         for v in self.validated:
             print v
             self.client.send('\x00' + v + '\xff')
 
+    def display(self):
+        img_name = "%s_%s.png" % (self.phi_list[self.phi],
+                                  self.theta_list[self.theta])
+
+        print '(%s, %s) [%s]' % (self.phi_list[self.phi],
+                                 self.theta_list[self.theta], img_name)
+
+        img = cv.LoadImage("Images/up/%s" % img_name)
+        cv.ShowImage("DIP", img)
+        cv.WaitKey(1000)
+
 
 def main():
     dip = DIPDemo()
-    dip.connect()
-    dip.recv()
-    dip.send()
+    dip.display()
+    dip.start()
+    while True: # Temporarily
+        dip.connect()
+        dip.recv()
+        dip.display()
+        dip.close()
 
 
 if __name__ == '__main__':
